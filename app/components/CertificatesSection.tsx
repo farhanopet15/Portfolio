2cd6241e215f75db3440fed6a734e94d5d9593bc
farhanopet15 <opet.mxcc@gmail.com>
2025-12-25 00:01:14 +0700

    fixing picture

--- app/components/CertificatesSection.tsx
+++ app/components/CertificatesSection.tsx
@@ -37,20 +37,20 @@ const certificates: Certificate[] = [
       "Full-stack web development certification focusing on JavaScript, React, Node.js, and deployment workflows — emphasizing modern coding standards, Git collaboration, and scalable app development.",
   },
   {
-    title: "E-IMBSkill",
+    title: "E-IBMSkill",
     issuer: "International Business Machines Corporation",
     year: "2025",
     image: "/E-IMBSkill.png",
     description:
-      "IBM Grenite Models for Software Development",
+      "Use Generate AI for Software Development",
   },
   {
-    title: "E-IMBSkill",
+    title: "E-IBMSkill",
     issuer: "International Business Machines Corporation",
     year: "2025",
     image: "/E-IMBSkill1.png",
     description:
-      "Use Generate AI for Software Development",
+      "IBM Granite Models for Software Development",
   },
 ];
 
